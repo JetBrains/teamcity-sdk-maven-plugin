06d47715b67b0b28d6a714e5afeb5e984b7d762b
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2014-04-12 22:15:45 +0400

    rename asserts

--- src/test/kotlin/UtilsTest.kt
+++ src/test/kotlin/UtilsTest.kt
@@ -11,24 +11,24 @@ import org.junit.Test
 public class UtilsTest {
 
     Test public fun testGetBitExtension() {
-        assertOne(1.getBit(0))
-        assertZero(1.getBit(1))
+        assertIsOne(1.getBit(0))
+        assertIsZero(1.getBit(1))
 
-        assertOne(3.getBit(0))
-        assertOne(3.getBit(1))
+        assertIsOne(3.getBit(0))
+        assertIsOne(3.getBit(1))
 
-        assertZero(Integer.MAX_VALUE.getBit(31))
-        assertOne(Integer.MAX_VALUE.getBit(0))
-        assertOne(Integer.MAX_VALUE.getBit(30))
+        assertIsZero(Integer.MAX_VALUE.getBit(31))
+        assertIsOne(Integer.MAX_VALUE.getBit(0))
+        assertIsOne(Integer.MAX_VALUE.getBit(30))
 
-        assertOne(Integer.MIN_VALUE.getBit(31))
+        assertIsOne(Integer.MIN_VALUE.getBit(31))
     }
 
-    private fun assertOne(actual: Int) {
+    private fun assertIsOne(actual: Int) {
         assertEquals(1, actual)
     }
 
-    private fun assertZero(actual: Int) {
+    private fun assertIsZero(actual: Int) {
         assertEquals(0, actual)
     }
 }
